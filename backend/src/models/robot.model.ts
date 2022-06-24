368623aaa3df982187464739398c61505c03f688
Irene <iavivas3@gmail.com>
2022-06-24 18:44:29 +0200

    Create robot model

---
+++ backend/src/models/robot.model.ts
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+import mongoose from 'mongoose';
+
+export interface iRobot {
+    id: number;
+    name: string;
+    image: string;
+    speed: number;
+    life: boolean;
+    dateEst: string;
+}
+
+const robotSchema = new mongoose.Schema({
+    id: Number,
+    name: String,
+    image: String,
+    speed: { type: Number, min: 0, max: 10 },
+    life: { type: Number, min: 0, max: 10 },
+    dateEst: String,
+});
+
+export const Robot = mongoose.model('Robot', robotSchema);
