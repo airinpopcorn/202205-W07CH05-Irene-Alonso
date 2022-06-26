1d7bdc2d1fbfb34649365573cb5b9a5782c0fa74
Irene <iavivas3@gmail.com>
2022-06-26 21:43:25 +0200

    Add delete functionality to card

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -1,15 +1,25 @@
 import { iRobot } from "../../models/robot";
 import { Link } from "react-router-dom";
+import * as ac from "../../reducers/robot/actions.creators";
+import { HttpStoreRobots } from "../../services/http.store.robot";
+import { useDispatch } from "react-redux";
 
 export function CardRobot({ robot }: { robot: iRobot }) {
+  const api = new HttpStoreRobots();
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    api.deleteRobot(robot._id).then(() => {
+      dispatch(ac.deleteRobot(robot));
+    });
+  };
   return (
     <>
       <Link to={"../details/" + robot._id}>
         <img src={robot.image} alt={`Robot ${robot.name}`} />
       </Link>
-      <img src={robot.image} alt={`Robot ${robot.name}`} />
       <p>{robot.name}</p>
-      <button>Borrar</button>
+      <button onClick={() => handleDelete()}>Borrar</button>
     </>
   );
 }
