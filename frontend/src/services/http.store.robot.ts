2b8feb8edf414806b08385ee20ba91d254d8a5a3
Irene <iavivas3@gmail.com>
2022-06-26 12:55:28 +0200

    Add http.store

---
+++ frontend/src/services/http.store.robot.ts
@@ -0,0 +1,44 @@
+import { iRobot } from "../models/robot";
+
+export class HttpStoreRobots {
+  apiUrl: string;
+  constructor() {
+    this.apiUrl = "http://localhost:3400/robots";
+  }
+
+  getAllRobots(): Promise<Array<iRobot>> {
+    return fetch(this.apiUrl).then((resp) => {
+      return resp.json();
+    });
+  }
+
+  getRobot(id: iRobot["id"]): Promise<iRobot> {
+    return fetch(this.apiUrl + String(id)).then((resp) => resp.json());
+  }
+
+  setRobot(robot: iRobot): Promise<iRobot> {
+    return fetch(this.apiUrl, {
+      method: "POST",
+      body: JSON.stringify(robot),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((resp) => resp.json());
+  }
+
+  updateRobot(robot: iRobot): Promise<iRobot> {
+    return fetch(this.apiUrl + robot.id.toString(), {
+      method: "PATCH",
+      body: JSON.stringify(robot),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((resp) => resp.json());
+  }
+
+  deleteRobot(id: iRobot["id"]): Promise<number> {
+    return fetch(this.apiUrl + String(id), {
+      method: "DELETE",
+    }).then((resp) => resp.json());
+  }
+}
